20ae86cb7b43754a6111becb96d1e6ef0ff6ec8e
felepas <pchfelipe09@gmail.com>
2026-06-09 16:37:13 -0300

    Lista 2 ex

--- Lista 2/lista2ex11.py
+++ Lista 2/lista2ex11.py
@@ -14,3 +14,48 @@
 #7 Iogurte R$ 6,50
 #8 Água R$ 2,50
 
+totalcaixa = 0
+
+while True:
+    codigo = int(input("digite o código do produto (0 para sair): "))
+
+    if codigo == 0:
+        break
+
+    quantidade = int(input("digite a quantidade: "))
+
+    if codigo == 1:
+        produto = "suco"
+        preco = 6.00
+    elif codigo == 2:
+        produto = "pão de queijo"
+        preco = 3.00
+    elif codigo == 3:
+        produto = "pastel"
+        preco = 7.00
+    elif codigo == 4:
+        produto = "salada de frutas"
+        preco = 9.00
+    elif codigo == 5:
+        produto = "café com leite"
+        preco = 3.50
+    elif codigo == 6:
+        produto = "cappuccino"
+        preco = 4.50
+    elif codigo == 7:
+        produto = "iogurte"
+        preco = 6.50
+    elif codigo == 8:
+        produto = "água"
+        preco = 2.50
+    else:
+        print("código inválido!")
+        continue
+
+    valorcompra = preco * quantidade
+    totalcaixa += valorcompra
+
+    print("produto:", produto)
+    print("valor da compra: R$", valorcompra)
+
+print("\nvalor total acumulado no caixa: R$", totalcaixa)
